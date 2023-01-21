04a4c17cbb7299557007d301b204a10644c9b8f2
Anna Ivakhnik <annaivakhnik@Annas-MacBook-Pro.local>
2023-01-21 13:58:53 -0500

    created div

--- client/App.jsx
+++ client/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import Home from './components/Home.jsx';
+
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>softI</h1>
+        <Home />
       </div>
     );
   }
---
+++ client/components/QuestionBox.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class QuestionBox extends Component {
+  render() {
+    return (
+      <div>
+        <p>
+          We will write questions here!
+        </p>
+      </div>
+    );
+  }
+}
+
+
+
+
+export default QuestionBox;
